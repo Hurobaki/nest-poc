d9f94139cbc6eff2e196e4ff3b875f5e82ebe1b5
Angélique Zheng <angelique.zheng@viseo.com>
2023-02-06 15:06:22 +0100

    Add swagger

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,20 @@
 import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
     const app = await NestFactory.create(AppModule);
+
+    const config = new DocumentBuilder()
+        .setTitle('Celine POC API')
+        .setDescription('')
+        .setVersion('0.1')
+        .addTag('Users')
+        .build();
+
+    const document = SwaggerModule.createDocument(app, config);
+    SwaggerModule.setup('swagger', app, document);
+
     await app.listen(3000);
 }
 bootstrap();
