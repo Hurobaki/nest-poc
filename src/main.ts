096f4487caf93dbad63b9c6bfa42eda696ce3d69
Angélique Zheng <angelique.zheng@viseo.com>
2023-02-08 12:00:57 +0100

    Add SwaggerDocumentOptions

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,7 @@
 import { NestFactory } from '@nestjs/core';
-import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { DocumentBuilder, SwaggerDocumentOptions, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
+import { TestDTO } from './users/dto/test.dto';
 
 async function bootstrap() {
     const app = await NestFactory.create(AppModule);
@@ -13,7 +14,12 @@ async function bootstrap() {
         .addTag('Users')
         .build();
 
-    const document = SwaggerModule.createDocument(app, config);
+    const options: SwaggerDocumentOptions = {
+        // Extra models that should be inspected and included in the specification
+        extraModels: [TestDTO]
+    };
+
+    const document = SwaggerModule.createDocument(app, config, options);
     SwaggerModule.setup('swagger', app, document);
 
     await app.listen(3000);
