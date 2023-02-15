65c0ec574a135c119a8dc866bac0856aebc2c130
Martin Prieur <martin.prieur@outlook.fr>
2023-02-15 14:17:18 +0100

    Add isPublic type

--- src/auth/guard/at.guard.ts
+++ src/auth/guard/at.guard.ts
@@ -1,6 +1,7 @@
 import { ExecutionContext, Injectable } from '@nestjs/common';
 import { Reflector } from '@nestjs/core';
 import { AuthGuard } from '@nestjs/passport';
+import { IS_PUBLIC_KEY, IS_PUBLIC_KEY_TYPE } from '../../utils/decorators/Public';
 
 /**
  * AuthGuard takes the name given to PassportStrategy as a parameter
@@ -13,7 +14,10 @@ export class AtGuard extends AuthGuard('jwt') {
 
     canActivate(context: ExecutionContext) {
         // Check if the endpoint has 'isPublic' metadata
-        const isPublic = this.reflector.getAllAndOverride('isPublic', [context.getHandler(), context.getClass()]);
+        const isPublic = this.reflector.getAllAndOverride<IS_PUBLIC_KEY_TYPE>(IS_PUBLIC_KEY, [
+            context.getHandler(),
+            context.getClass()
+        ]);
 
         return isPublic || super.canActivate(context);
     }
--- src/utils/decorators/Public.ts
+++ src/utils/decorators/Public.ts
@@ -1,4 +1,5 @@
 import { SetMetadata } from '@nestjs/common';
 
 export const IS_PUBLIC_KEY = 'isPublic';
-export const Public = () => SetMetadata(IS_PUBLIC_KEY, true);
+export type IS_PUBLIC_KEY_TYPE = boolean;
+export const Public = () => SetMetadata<string, IS_PUBLIC_KEY_TYPE>(IS_PUBLIC_KEY, true);
